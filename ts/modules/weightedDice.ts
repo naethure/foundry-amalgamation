c83602dfed8736a75690d61d28f6cc70a0d85ed3
Naethure <naethure@gmail.com>
2021-02-08 03:37:30 -0800

    Switched to run on Ready instead of Init

--- ts/modules/weightedDice.ts
+++ ts/modules/weightedDice.ts
@@ -9,7 +9,7 @@ declare const CONFIG: any;
 const enabledSetting = new Settings.BooleanSetting("WeightedDice.Enabled", FoundryInterop.Localization.localize("Naethure.Settings.WeightedDice.Enabled.Title"), FoundryInterop.Localization.localize("Naethure.Settings.WeightedDice.Enabled.Description"), Settings.SettingScope.World, true);
 const weightedDiceModule = new Modules.Module("Weighted Dice", enabledSetting, true, true);
 
-weightedDiceModule.RegisterHook(Constants.Hooks.Init, () => {
+weightedDiceModule.RegisterHook(Constants.Hooks.Ready, () => {
     DiceTerm.prototype.roll = function({minimize=false, maximize=false}={}) {
         const d20Weights: Array<number> = [.0200, .0240, .0280, .0320, .0370, .0425, .0540, .0625, .0710, .0800, .0825, .0720, .0650, .0570, .0520, .0485, .0460, .0440, .0420, .0400];
         var rand = CONFIG.Dice.randomUniform();
