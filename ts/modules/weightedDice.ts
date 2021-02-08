eb0f01669b4a0e58f16af43c1eec9ce5c36ed85c
Naethure <naethure@gmail.com>
2021-02-08 04:34:41 -0800

    Added setting.Register()

--- ts/modules/weightedDice.ts
+++ ts/modules/weightedDice.ts
@@ -9,6 +9,8 @@ declare const CONFIG: any;
 FoundryInterop.HookManager.once(Constants.Hooks.Init, () => {
 
     const enabledSetting = new Settings.BooleanSetting("WeightedDice.Enabled", FoundryInterop.Localization.localize("Naethure.Settings.WeightedDice.Enabled.Title"), FoundryInterop.Localization.localize("Naethure.Settings.WeightedDice.Enabled.Description"), Settings.SettingScope.World, true);
+    enabledSetting.Register();
+    
     const weightedDiceModule = new Modules.Module("Weighted Dice", enabledSetting, true, true);
 
     weightedDiceModule.RegisterHook(Constants.Hooks.Ready, () => {
